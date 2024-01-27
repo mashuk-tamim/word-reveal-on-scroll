c136eb4fd77f5dd68e5297f9c8ec725c13150adb
mashuk-tamim <tamimmashuk328@gmail.com>
2024-01-27 22:53:29 +0600

    revealing character by character

---
+++ app/components/Characters.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Lenis from "@studio-freight/lenis";
+
+type Props = {
+	paragraph: string;
+};
+
+export default function Characters({ paragraph }: Props) {
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["0 0.9", "0 0.25"],
+	});
+	const words = paragraph.split(" ");
+
+	return (
+		<p ref={ref} style={{}} className="flex flex-wrap">
+			{words.map((word, i) => {
+				const start = i / words.length;
+				const end = start + 1 / words.length;
+				// console.log([start, end]);
+				const range = [start, end];
+				return (
+					<Word
+						range={range}
+						scrollYProgress={scrollYProgress}
+						key={i}
+					>
+						{word}
+					</Word>
+				);
+			})}
+		</p>
+	);
+}
+type wordProps = {
+	children: string;
+	range: number[];
+	scrollYProgress: import("framer-motion").MotionValue<number>;
+};
+const Word = ({ children, range, scrollYProgress }: wordProps) => {
+	const characters = children.split("");
+	const amount = range[1] - range[0];
+	const step = amount / children.length;
+	return (
+		<span className="relative mr-3 mt-3">
+			{characters.map((character, i) => {
+				const start = range[0] + i * step;
+				const end = range[0] + (i + 1) * step;
+				// console.log([start, end])
+				return (
+					<Character
+						range={[start, end]}
+						scrollYProgress={scrollYProgress}
+						key={i}
+					>
+						{character}
+					</Character>
+				);
+			})}
+		</span>
+	);
+};
+
+type CharacterProps = {
+	children: string;
+	range: number[];
+	scrollYProgress: import("framer-motion").MotionValue<number>;
+};
+const Character = ({ children, range, scrollYProgress }: CharacterProps) => {
+	const opacity = useTransform(scrollYProgress, range, [0, 1]);
+	return (
+        <span className="relative">
+            <span className="absolute opacity-10">{children}</span>
+			<motion.span
+				style={{
+					opacity,
+				}}
+			>
+				{children}
+			</motion.span>
+		</span>
+	);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,43 @@ import Paragraph from "./components/Paragraph";
 import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
 import Words from "./components/Words";
+import Characters from "./components/Characters";
 const paragraph =
 	"Hello, I'm Mashuk Tamim, an Associate Software Engineer at Uqidev. My passion lies in crafting seamless user interfaces, staying at the forefront of industry trends to deliver innovative solutions. Let's shape the future of the web together!";
 
 export default function Home() {
-    useEffect(() => {
-        const lenis = new Lenis();
-		function raf(time:any ) {
+	useEffect(() => {
+		const lenis = new Lenis();
+		function raf(time: any) {
 			lenis.raf(time);
 			requestAnimationFrame(raf);
 		}
 		requestAnimationFrame(raf);
-    },[])
+	}, []);
 	return (
-		<main className="max-w-7xl mx-auto text-4xl p-5 tracking-wide text-justify">
-			<div style={{ height: "100vh" }}></div>
+		<main className="max-w-7xl mx-auto text-4xl p-5 tracking-wide ">
+			<div
+				style={{ height: "100vh" }}
+				className="flex justify-center items-center"
+			>
+				<h1 className="text-6xl font-bold">Scroll Down</h1>
+			</div>
 			<Paragraph paragraph={paragraph}></Paragraph>
-            <div className="h-[100vh]"></div>
-            <Words paragraph={paragraph}></Words>
-            <div className="h-[100vh]"></div>
+			<div
+				style={{ height: "100vh" }}
+				className="flex justify-center items-center"
+			>
+				<h1 className="text-7xl font-bold">Words by Words</h1>
+			</div>
+			<Words paragraph={paragraph}></Words>
+			<div
+				style={{ height: "100vh" }}
+				className="flex justify-center items-center"
+			>
+				<h1 className="text-6xl font-bold">Characters by Characters</h1>
+			</div>
+			<Characters paragraph={paragraph}></Characters>
+			<div className="h-[100vh]"></div>
 		</main>
 	);
 }
